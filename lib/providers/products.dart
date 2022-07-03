8ed26fafbcb5893fe02aa1682457e1801d673a61
森田陸斗 <mrmrt1121@gmail.com>
2022-07-03 23:27:43 +0900

    install a provider package

---
+++ lib/providers/products.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+import '../models/product.dart';
+
+
+class Products with ChangeNotifier {
+  List<Product> _items = [];
+
+  List<Product> get item {
+    return [..._items];
+  }
+
+  void addProduct() {
+    // _items.add(value);
+    notifyListeners();
+  }
+}
