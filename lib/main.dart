cc64c248c511f0a82e43c5ab72b4671a4e5ca8a1
森田陸斗 <mrmrt1121@gmail.com>
2022-07-05 17:33:27 +0900

    add a route to CartPage in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import './config/custom_theme_data.dart';
-import '../pages/products_overview_page.dart';
+import './pages/products_overview_page.dart';
 import './pages/product_detail_page.dart';
 import './providers/products.dart';
 import './providers/cart.dart';
+import './pages/cart_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -30,7 +31,10 @@ class MyApp extends StatelessWidget {
         theme: customLightThemeData,
         darkTheme: customDarkThemeData,
         home: const ProductOverViewPage(),
-        routes: {ProductDetailPage.routeName: (context) => ProductDetailPage()},
+        routes: {
+          ProductDetailPage.routeName: (context) => const ProductDetailPage(),
+          CartPage.routeName: (context) => const CartPage(),
+        },
       ),
     );
   }
--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -5,6 +5,7 @@ import '../providers/cart.dart';
 
 class CartPage extends StatelessWidget {
   const CartPage({Key? key}) : super(key: key);
+  static const routeName = '/cart';
 
   @override
   Widget build(BuildContext context) {
